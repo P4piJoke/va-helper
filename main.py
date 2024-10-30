f145010b2b68f1155f2150de91c9a954642f4389
Danylo Papizhuk <danil.papizhuk02@gmail.com>
2024-10-30 02:37:40 +0200

    Face auth

--- main.py
+++ main.py
@@ -4,11 +4,30 @@ import eel
 
 from engine.features import *
 from engine.command import *
+from engine.auth import recognize
 
 def start():
     eel.init("www")
 
     playAssistantSound()
+    @eel.expose
+    def init():
+        eel.hideLoader()
+        speak('Ready for Face Authentication')
+        flag = recognize.AuthenticateFace()
+        
+        if (flag == 1):
+            eel.hideFaceAuth()
+
+            speak('Authentication is sucessful')
+            eel.hideFaceAuthSuccess()
+
+            speak('You are welcome, how can I help you?')
+            eel.hideStart()
+
+            playAssistantSound()
+        else:
+            speak('Authentication is failed')
 
     os.system('start msedge.exe -app="http:/localhost:8000/index.html"')
 
--- www/controller.js
+++ www/controller.js
@@ -6,7 +6,7 @@ $(document).ready(function () {
     }
 
     eel.expose(ShowHood)
-    function ShowHood(){
+    function ShowHood() {
         $("#Oval").attr("hidden", false);
         $("#SiriWave").attr("hidden", true);
     }
@@ -18,8 +18,8 @@ $(document).ready(function () {
             chatBox.innerHTML += `<div class="row justify-content-end mb-4">
             <div class = "width-size">
             <div class="sender_message">${message}</div>
-        </div>`; 
-    
+        </div>`;
+
             // Scroll to the bottom of the chat box
             chatBox.scrollTop = chatBox.scrollHeight;
         }
@@ -34,10 +34,51 @@ $(document).ready(function () {
             <div class = "width-size">
             <div class="receiver_message">${message}</div>
             </div>
-        </div>`; 
-    
+        </div>`;
+
             // Scroll to the bottom of the chat box
             chatBox.scrollTop = chatBox.scrollHeight;
         }
+    }
+
+    // Hide Loader and display Face Auth animation
+    eel.expose(hideLoader)
+    function hideLoader() {
+
+        $("#Loader").attr("hidden", true);
+        $("#FaceAuth").attr("hidden", false);
+
+    }
+    // Hide Face auth and display Face Auth success animation
+    eel.expose(hideFaceAuth)
+    function hideFaceAuth() {
+
+        $("#FaceAuth").attr("hidden", true);
+        $("#FaceAuthSuccess").attr("hidden", false);
+
+    }
+    // Hide success and display 
+    eel.expose(hideFaceAuthSuccess)
+    function hideFaceAuthSuccess() {
+
+        $("#FaceAuthSuccess").attr("hidden", true);
+        $("#HelloGreet").attr("hidden", false);
+
+    }
+
+
+    // Hide Start Page and display blob
+    eel.expose(hideStart)
+    function hideStart() {
+
+        $("#Start").attr("hidden", true);
+
+        setTimeout(function () {
+            $("#Oval").addClass("animate__animated animate__zoomIn");
+
+        }, 1000)
+        setTimeout(function () {
+            $("#Oval").attr("hidden", false);
+        }, 1000)
     }
 });
